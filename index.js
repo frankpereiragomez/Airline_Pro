f69d93db30471e8c7394c4fc7b6c5a137d58ebf8
Aragata000 <frankpereiragomez@franks-macbook-pro.home>
2022-09-16 19:26:15 +0200

    Airline simple version

--- index.js
+++ index.js
@@ -12,25 +12,6 @@
                 
  */
 
-/*
-  Después de ver toda la información, el programa preguntará al usuario si es ADMIN/USER.
-  - Si introduce algo diferente a ADMIN/USER o no introduce nada, mostramos un alert pidiéndole que se identifique como uno de los dos.
-    -> USER: El usuario podrá buscar por precio, le mostramos los vuelos que tengan ese precio o más barato.
-        - Si introduce algo que no sean números o deja el campo vacio, le mostramos un alert pidiéndole que solo introduzca números.
-    -> Le preguntamos si desea hacer otra busqueda por precios
-        -> Si: Repetir paso anterior(buscar vuelos por precio)
-        -> No: Pasar al sguiente paso(mostrar los últimos cinco vuelos del día)
-    -> ADMIN: El usuario puede crear/eliminar(por id) más vuelos, pidiendo la información por prompt().
-        -> Crear:  
-            - Si el usuario no introduce datos o introduce datos incorrectos se le pedirá con un alert() que solo introduzca los datos pertinentes
-      -> Preguntar al usuario si desea crear otro vuelo
-            Si: Repetir el paso anterior.
-                - No podrá pasar de 15 vuelos, si se intenta introducir uno más, saltará un alert().
-            No: Pasar al siguiente paso(mostrar los últimos cinco vuelos del día)
- -> Cerrar programa(Despedir al usuario).                  
-
-            
-   */
 const flights = [
   { id: 00, to: "New York", from: "Barcelona", cost: 700, scale: false },
   { id: 01, to: "Los Angeles", from: "Madrid", cost: 1100, scale: true },
@@ -76,66 +57,6 @@ const flightsMeanCost = () => {
   return totalCost / flights.length;
 };
 
-const askRole = () => {
-  let role = prompt("Are you an user or administrator? (admin/user)");
-  if (
-    !role ||
-    (role.toLowerCase() !== "admin" && role.toLowerCase() !== "user")
-  ) {
-    askRole();
-  } else if (role.toLowerCase() === "admin") {
-    return adminActions();
-  } else if (role.toLowerCase() === "user") {
-    return userAction();
-  }
-};
-
-const userAction = () => {
-  const maxCost = +prompt("Enter your maximum cost");
-  if (!maxCost || isNaN(maxCost)) {
-    alert(" Please, insert only a number!");
-    userAction();
-  } else {
-    console.log(`The flights with a maximum cost of ${maxCost} are: `);
-    flights.forEach((flight) => {
-      if (flight.cost <= maxCost) {
-        console.log(
-          `The flight with origin: ${flight.from}, and destination: ${
-            flight.to
-          } has a cost of ${flight.cost} and ${
-            flight.scale ? "make stopover" : "does not make any stopover."
-          }`
-        );
-      }
-    });
-  }
-};
-
-const createFlightsById = () => {
-  let askId = +prompt("Introduce new id");
-  if (!askId || isNaN(askId)) {
-    alert("Please, introduce only a number id !");
-    createFlightsById();
-  } else {
-    return askId;
-  }
-};
-
-const createFlights = () => {};
-
-const lastUserCuestion = () => {
-  let cuestion = prompt("Want to find other flights by cost ? (yes/no)");
-  if (
-    !cuestion ||
-    (cuestion.toLowerCase() !== "yes" && cuestion.toLowerCase() !== "no")
-  ) {
-    alert("Please, only chose: yes or no.");
-    lastUserCuestion();
-  } else if (cuestion.toLowerCase() === "yes") {
-    return userAction();
-  }
-};
-
 const init = () => {
   let name = askNameAndGreet();
   console.log("****** Our flights ****** : ");
@@ -143,8 +64,6 @@ const init = () => {
   console.log("*************************");
   let mean = flightsMeanCost();
   console.log(`****** The average cost of our flights is: ${mean} € ******`);
-  askRole();
-  lastUserCuestion();
   console.log("*************************");
   const showScale = confirm("Do you want to see flights with stopovers?");
   showScale && showFlights((flight) => flight.scale);
